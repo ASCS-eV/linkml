2bbb3412eb61a86625b3c2bb10da9426e312648c
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2026-04-17 15:57:05 +0300

    Run pre-commit

--- packages/linkml/src/linkml/generators/owlgen.py
+++ packages/linkml/src/linkml/generators/owlgen.py
@@ -18,8 +18,6 @@ from rdflib.namespace import RDFS, SKOS
 from rdflib.plugin import Parser as rdflib_Parser
 from rdflib.plugin import plugins as rdflib_plugins
 
-from linkml_runtime.utils.rdf_canonicalize import canonicalize_rdf_graph
-
 from linkml import METAMODEL_NAMESPACE_NAME
 from linkml._version import __version__
 from linkml.generators.common.subproperty import is_xsd_anyuri_range
@@ -45,6 +43,7 @@ from linkml_runtime.linkml_model.meta import (
 )
 from linkml_runtime.utils.formatutils import camelcase, underscore
 from linkml_runtime.utils.introspection import package_schemaview
+from linkml_runtime.utils.rdf_canonicalize import canonicalize_rdf_graph
 
 logger = logging.getLogger(__name__)
 
